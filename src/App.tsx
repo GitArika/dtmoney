1318ae264c3255cfb1a31b0b9622392f02e6eb26
Ariel Evangelista <ariel.evangelista@outlook.com>
2022-04-17 20:25:46 -0300

    docs: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GlobalStyle } from './styles/Global';
+
+export function App() {
+  return (
+    <div className="App">
+      <h1 >Hello World</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
+
