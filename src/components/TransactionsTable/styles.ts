3a4064894c7cd70cbf4bd2a6bb0c9de7bc1799a9
Ariel Evangelista <ariel.evangelista@outlook.com>
2022-04-19 18:54:21 -0300

    feat: transactions table

---
+++ src/components/TransactionsTable/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin-top: 4rem;
+
+
+    table {
+        width: 100%;
+
+        border-spacing: 0 0.5rem;
+
+        th  {
+            font-weight: 400;
+            color: var(--text-body);            
+            
+            padding: 1rem 2rem;
+            
+            line-height: 1.5rem;            
+            text-align: left;
+        }
+
+        td { 
+            padding: 1rem 2rem;
+            
+            background: var(--shape);
+            
+            color: var(--text-body);    
+
+            border: 0;
+            border-radius: 0.25rem;
+
+            &:first-child { 
+                color: var(--text-title);    
+            }
+
+            &.deposit { 
+                color: var(--green);
+            }
+
+            &.withdraw { 
+                color: var(--red);
+            }
+            
+            
+        }
+    }
+`;
